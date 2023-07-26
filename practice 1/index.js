6742daad7ef847335e8046edbbf06d6d6ed4d342
Tomilade18 <jboyyumzey@gmail.com>
2023-07-26 23:38:18 +0100

    second commiit

---
+++ practice 1/index.js
@@ -0,0 +1,15 @@
+//type conversion
+let y = "5";
+console.log(y)
+
+let x = + y;
+
+console.log(x)
+//
+//arrow function
+// save = document.getElementById("save-btn")
+
+addEventListener
+function save() {
+    console.log("saved")
+}
---
+++ practice 2/index.js
@@ -0,0 +1,4 @@
+// Strings
+let x = document.getElementById("input")
+ x.innerText = "this is the first line\nAnd this the second"
+console.log(x)
