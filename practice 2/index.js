50ccddc047bb7392c9a69001a8aa26bd5c05bfb1
Tomilade18 <jboyyumzey@gmail.com>
2023-07-26 23:57:32 +0100

    concate

--- practice 2/index.js
+++ practice 2/index.js
@@ -2,3 +2,7 @@
 let x = document.getElementById("input")
  x.innerText = "this is the first line\nAnd this the second"
 console.log(x)
+
+// concatenate
+let yes = "I"+" have"+" passed"
+console.log(yes)
